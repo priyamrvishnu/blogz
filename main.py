9d8fb82d9dd4b1a6b7b7d377a8a1f05d4516f1c8
Vishnupriya <priyamrvishnu@gmail.com>
2018-11-29 15:26:49 -0600

    validation added

--- main.py
+++ main.py
@@ -27,12 +27,14 @@ class Blog(db.Model):
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(120))
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    tasks= db.relationship('Blog', backref ='owner')
+    tasks= db.relationship('Blog', backref ='owner', lazy='dynamic')
 
-    def __init__(self, email, password):
+    def __init__(self, username, password, email):
         self.email = email
+        self.username=username
         self.password = password
 
 @app.before_request
@@ -54,27 +56,111 @@ def login():
             return redirect('/todos')
         else:
             flash('User password incorrect or user does not exists', 'error')
-            
 
     return render_template('login.html')
 
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    if request.method=='POST':
-        email= request.form ['email']
-        password = request.form ['password']
-        verify = request.form['verify']
-        existing_user= User.query.filter_by(email=email).first()
-        if not existing_user:
-            new_user=User(email, password)
-            db.session.add(new_user)
-            db.session.commit()
-            session['email']=email
-            return redirect('/')
-        else:
-            #TODO user better response messaging
-            return "<h1>Duplicate User </h1>"
-    return render_template('signup.html')           
+    username = request.form['username']
+    passwd = request.form['password']
+    confi_pass = request.form['verify']
+    e_mail = request.form['email']
+
+    username_error = ''
+    passwd_error = ''
+    confi_pass_error=''
+    e_mail_error=''
+
+    numupper =0
+    numlower =0
+    numdigit=0
+    for c in username:
+        if c.isupper():
+            numupper = numupper + 1
+        if c.islower():
+            numlower = numlower + 1
+        if c.isdigit():
+            numdigit = numdigit + 1
+
+
+    if numupper <= 0:
+        username_error=('username must contain at least one uppercase character')
+        
+       
+    if numlower <= 0:
+        username_error=('username must contain at least one lowercase character')
+        
+
+    if len(username)<6:
+        username_error = ('username must be greater than 6 characters')
+        
+          
+    if numdigit <= 0:
+        username_error= ('username must contain at least one number')
+           
+
+    numupper =0
+    numlower =0
+    numdigit=0
+    for c in passwd:
+        if c.isupper():
+           numupper = numupper + 1
+        if c.islower():
+            numlower = numlower + 1
+        if c.isdigit():
+            numdigit = numdigit + 1
+
+    if numupper <= 0:
+        passwd_error=('password must contain at least one uppercase character')
+        passwd=''
+
+    elif numlower <= 0:
+        passwd_error=('password must contain at least one lowercase character')
+        passwd=''
+
+    elif len(passwd)<8:
+        passwd_error = ('password must be greater than 8 characters')
+        passwd=''
+
+    else:
+        if numdigit <= 0:
+            passwd_error= ('password must contain at least one number')
+            passwd=''
+    
+    if confi_pass == passwd:
+        confi_pass=''
+    else:
+        confi_pass_error= ('Password does not Match')   
+
+    if '@' not in e_mail or '.com' not in e_mail:
+        e_mail_error=('Not a valid email')
+
+
+
+    if not username_error and not passwd_error and not confi_pass_error and not e_mail_error:
+        return render_template('login.html', name= username)
+    else:
+        return redirect('/?username_error='+username_error+
+        '&passwd_error='+passwd_error+
+        '&confi_pass_error='+confi_pass_error+
+        '&e_mail_error='+e_mail_error+
+        '&username='+username+
+        '&passwd='+passwd+
+        '&confi_pass='+confi_pass+
+        '&e_mail='+e_mail)   
+
+    existing_user= User.query.filter_by(email=email).first()
+    if not existing_user:
+        new_user=User(username,password,email)
+        db.session.add(new_user)
+        db.session.commit()
+        session['email']=email
+        return redirect('/')
+    else:
+        flash("You already have an account. Please login")
+        return redirect('/login')  
+    return render_template('signup.html',pgtitle="Signup")           
     
     
 @app.route('/logout', methods=['GET'])
@@ -122,19 +208,19 @@ def add_blog():
         return render_template ('todos.html', title= "Add a Blog", title_error=title_error, 
         blog_content_error=blog_content_error)
 
-@app.route('/', defaults={'id':0,'email':''})
-@app.route('/display/<int:id>',defaults={'email':''})
-@app.route('/display/<email>',defaults={'id':0})
-def index(id, email):
+@app.route('/', defaults={'id':0,'username':''})
+@app.route('/display/<int:id>',defaults={'username':''})
+@app.route('/display/<username>',defaults={'id':0})
+def index(id, username):
     user_name=User.query.filter_by().all()
     if id:
         update_blog = Blog.query.get(id)
         return render_template('display.html',title="Details of Blogs", 
          update_blog=update_blog )
     if email:
-        user_blog=User.query.filter_by(email=email).first()
-        update_blog=Blog.query.filter_by(owner=user_blog).first()
-        return render_template('singleUser.html',title="Add a Blog", update_blog=update_blog)
+        user_blog=User.query.filter_by(username=username).first()
+        tasks=Blog.query.filter_by(owner=user_blog).all()
+        return render_template('singleUser.html',title="Add a Blog", tasks=tasks)
     
     return render_template('first.html', title="Blog Users!", user_name=user_name)
 
